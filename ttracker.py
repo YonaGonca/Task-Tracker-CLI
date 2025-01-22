8fb099286e872647fb80c38457844a16e390c286
YonaGonca <ygoncalves5a@gmail.com>
2025-01-22 18:27:31 +0000

    Second commit: Creation of the CLI Structure

--- ttracker.py
+++ ttracker.py
@@ -1,1 +1,13 @@
-print("Hola")
+import argparse
+
+def addTask(task):
+    
+
+
+def main():
+    # Crear el parser principal
+    parser = argparse.ArgumentParser(description=" CLI Aplication to track your tasks")
+
+
+if __name__ == "__main__":
+    main()
