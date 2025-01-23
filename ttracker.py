59f8f3174ca8527ddba0bb2e74f1ce0ec93a925c
YonaGonca <ygoncalves5a@gmail.com>
2025-01-23 11:25:56 +0000

    5th commit: -Create a function to verify if te JSON file exists -Create two functions to read and write the JSON file -Create a function to add a parser that add tasks(Not done yet) -Create a function to add a parser that list tasks(Not done yet)

--- ttracker.py
+++ ttracker.py
@@ -1,13 +1,70 @@
 import argparse
+import json
+import os
 
-def addTask(task):
+# JSON Adress
+JSON_FILE = "data.json"
+
+# Function that verifies if the JSON file exists. If not, it creat it.
+def ensure_json_file_exists():
+    if not os.path.exists(JSON_FILE):
+        with open(JSON_FILE, "w") as file:
+            json.dump({"tasks": []}, file, indent=4)
+        print(f"'{JSON_FILE}' File created automatically.")
+
+# Function that read the JSON file
+def read_json():
+    ensure_json_file_exists()
+    with open(JSON_FILE, "r") as file:
+        return json.load(file)
+
+# Function that write into the JSON file
+def write_json(data):
+    with open(JSON_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+# Function that add a task to the JSON file
+def add_task(task):
+    data = read_json()
+    task_id = len(data["tasks"]) 
+    new_task = {"id": task_id, "description": task}
+    data["tasks"].append(new_task)
+    write_json(data)
+    print(f'Task "{task}" added successfully with ID = "{task_id}".')
+    
+# Function that lists all the tasks of the JSON file
+def list_tasks():
+    data = read_json()
+    for i in data["tasks"]:
+        print(i['id'],i['description'])
     
 
 
 def main():
     # Crear el parser principal
     parser = argparse.ArgumentParser(description=" CLI Aplication to track your tasks")
+    subparsers = parser.add_subparsers(dest="command", help="Comandos disponibles")
 
+    # Comando para añadir una tarea
+    add_parser = subparsers.add_parser("add", help="Añadir una nueva tarea")
+    add_parser.add_argument("task", type=str, help="Descripción de la tarea")
+    
+    # Comando para listar las tareas
+    list_parser = subparsers.add_parser("list", help="List all the tasks")
+
+    # Parsear los argumentos
+    args = parser.parse_args()
+
+    # Ejecutar el comando correspondiente
+    if args.command == "add":
+        add_task(args.task)
+    elif args.command == "list":
+        list_tasks()
+    elif args.command == "remove":
+        remove_task(args.index)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
+    ensure_json_file_exists()
     main()
